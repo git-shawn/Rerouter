e7876a6e1e4828fd50dde55ba86201e931154c42
Shawn <61984193+git-shawn@users.noreply.github.com>
2021-10-06 13:34:18 -0600

    Initial Commit

---
+++ Reroute/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Reroute
+//
+//  Created by Shawn Davis on 10/6/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
