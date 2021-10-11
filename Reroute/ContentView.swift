57a67081a14a5d767b915dcb1a46654a977cc251
Shawn <61984193+git-shawn@users.noreply.github.com>
2021-10-11 13:17:14 -0600

    Share App Button, Request Review, Popup Padding
    - Added padding to the popup for a better appearance on iPad
    - Added a Share Rerouter button to share the app
    - When a user shares the app, request a rating!

--- Reroute/ContentView.swift
+++ Reroute/ContentView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
     @State private var showExtension: Bool = false
     @State private var showPrivacy: Bool = false
+    @State private var showShare: Bool = false
+    private var sawRatingRequest: Bool = UserDefaults.standard.bool(forKey: "sawRatingRequest")
     
     var body: some View {
         NavigationView {
@@ -58,6 +61,30 @@ struct ContentView: View {
                                 .foregroundColor(.accentColor)
                         }
                     }
+                    Button(action: {
+                        self.showShare = true
+                    }) {
+                        Label {
+                            Text("Share Rerouter")
+                                .foregroundColor(.primary)
+                        } icon: {
+                            Image(systemName: "square.and.arrow.up.fill")
+                                .foregroundColor(.accentColor)
+                        }
+                    }
+                    .sheet(isPresented: $showShare, onDismiss: {
+                        // If they shared the app, request a review. If they've seen the request before, don't show it again.
+                        if (sawRatingRequest == false) {
+                            if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                                SKStoreReviewController.requestReview(in: scene)
+                                UserDefaults.standard.set(true, forKey: "sawRatingRequest")
+                            }
+                        }
+                    }, content: {
+                        ZStack {
+                        ActivityViewController(activityItems: [URL(string: "https://fromshawn.dev/rerouter.html")!])
+                        }.background(.ultraThickMaterial)
+                    })
                 }
             }.navigationTitle("Rerouter")
         }.navigationViewStyle(.stack)
