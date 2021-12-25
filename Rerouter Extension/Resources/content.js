96e06fa95dfe45b8147b1f883234e3f700b93980
Shawn <61984193+git-shawn@users.noreply.github.com>
2021-12-25 13:26:22 -0700

    Update content.js

--- Rerouter Extension/Resources/content.js
+++ Rerouter Extension/Resources/content.js
@@ -1,15 +1,1 @@
-function handleMessage(request, sender, sendResponse) {
-    if (request.type == "prepareLanding") {
-        console.log(request.amapsLink);
-        console.log(request.gmapsLink);
-        
-        var amapsBtn = document.getElementById("amapslink");
-        amapsBtn.href = request.amapsLink;
-        amapsBtn.style.display = "block";
-        var gmapsBtn = document.getElementById("gmapslink");
-        gmapsBtn.href = request.gmapslink
-        amapsBtn.style.display = "block"
-    }
-}
-
-browser.runtime.onMessage.addListener(handleMessage);
+// nothing
