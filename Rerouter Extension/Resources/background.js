a829815e2629e80900cb28d5bbe5edfdf4c8b087
Shawn <61984193+git-shawn@users.noreply.github.com>
2022-05-07 11:15:21 -0500

    Better link support
    - Rerouter now works more reliably when opened from links outside Google search
    - Fixes a bug related to Google 'data' links, where the coordinates are obfuscated behind private maps data

--- Rerouter Extension/Resources/background.js
+++ Rerouter Extension/Resources/background.js
@@ -1,15 +1,6 @@
-// Possible Google Maps hosts to watch for
-const filter = {
-  url:
-  [
-   {urlMatches: "(http(s?):\/\/)?((maps\.google\.[a-z]{1,}\/)|((www\.)?google\.[a-z]{1,}\/maps\/)|(goo.gl\/maps\/))+.*"}
-  ]
-}
-
 // Intercept Google Maps links before they are handled, then redirect them.
 function redirectPage(details) {
     // Check if the user has enabled automatic or manual mode.
-    console.log(details)
     browser.runtime.sendNativeMessage("shwndvs.Rerouter", {message: "getDefaults"}, function(response) {
         var redirectUrl = ""
 
@@ -19,20 +10,36 @@ function redirectPage(details) {
             
             address = address.split("/")[2];
             redirectUrl = "http://maps.apple.com/?daddr="+address
-
+        
+        // If the coordinates have been obscured behind hexadecimal code, try to parse it.
+        } else if ((details.url).includes('place//data=!')) {
+            var hexCode = (details.url).split(':').pop();
+            var hexCode = hexCode.split('?')[0];
+            let bigHex = BigInt(hexCode);
+            let cid = bigHex.toString(10);
+            
+            let rerouteNow = "https://google.com/maps?cid=" + cid;
+            browser.tabs.update({url: rerouteNow});
+            
         // If the link is for a place or search query, parse it.
         } else if ((details.url).includes('place') || (details.url).includes('search')) {
+            
             var place = (details.url).split('/maps/').pop();
+            if ((details.url).includes('@')) {
+                let coordinates = (place.split("@").pop()).split("/")[0];
 
-            let coordinates = (place.split("@").pop()).split("/")[0];
+                let lat = coordinates.split(",")[0];
+                let long = coordinates.split(",")[1];
+                let zoom = (coordinates.split(",")[2]).replace('z','');
 
-            let lat = coordinates.split(",")[0];
-            let long = coordinates.split(",")[1];
-            let zoom = (coordinates.split(",")[2]).replace('z','');
+                place = place.split("/")[1];
 
-            place = place.split("/")[1];
-
-            redirectUrl = "http://maps.apple.com/?q="+place+"&sll="+lat+","+long+"&z="+zoom
+                redirectUrl = "http://maps.apple.com/?q="+place+"&sll="+lat+","+long+"&z="+zoom
+            } else {
+                // If a place has no coordinates, just go to the location itself.
+                let locTitle = place.split("/")[1];
+                redirectUrl = "http://maps.apple.com/?address=" + locTitle;
+            }
         
         // If the link is just loose coordinates, parse it.
         } else {
@@ -44,40 +51,37 @@ function redirectPage(details) {
 
           redirectUrl = "http://maps.apple.com/?ll="+lat+","+long+"&z="+zoom;
         }
-
-        if (!response.manual) {
+        
+        if (!response.manual && redirectUrl != "") {
             // If automatic mode, update the tab to the new Apple Mapss link.
             browser.tabs.update({url: redirectUrl});
-//            browser.tabs.sendMessage(details.tabId, {redirect: redirectUrl});
         } else {
             // If manual mode, pass the parsed links to Rerouter's landing page then navigate.
-            if (details.url.includes("reroute=true") == false) {
+            if (details.url.includes("reroute=true") == false && redirectUrl != "") {
                 var landingURL = browser.runtime.getURL("landing.html");
                 landingURL = landingURL + "?aLink=" + redirectUrl + "&gLink=" + details.url + "&reroute=true";
-                console.log(landingURL)
-                browser.tabs.update({url: landingURL, loadReplace: true});
+                browser.tabs.update({url: landingURL});
             }
         }
     });
 }
 
-// Match the URL against the gmaps regex.
-function filterTabs(tab) {
-    let regex = /^(http(s?):\/\/)?((maps\.google\.[a-z]{1,}\/)|((www\.)?google\.[a-z]{1,}\/maps\/)|(goo.gl\/maps\/))+.*/
-    // Ensure this tab is a Google Maps page
-    if (regex.test(tab.url)) {
-        console.log("regex passed for " + tab.url)
-        // Redirect it
-        redirectPage(tab)
-    }
-}
+// Store the url to be changed to prevent infinite reloads
+var previousURL = "";
 
-function onError(error) {
-    console.log(error)
-}
-
-// Called when a user attemps to navigate to a gmaps link.
-browser.webNavigation.onBeforeNavigate.addListener(redirectPage, filter);
+// Master regex that determines what is and is not a Google maps link
+const regex = /(http(s?):\/\/)?((maps\.google\.[a-z]{1,}\/)|((www\.)?google\.[a-z]{1,}\/maps\/)|(goo.gl\/maps\/))+.*/;
 
 //Called when a user opens a gmaps link from outside of the browser.
-browser.tabs.onCreated.addListener(filterTabs)
+browser.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
+    if ((changeInfo.status || tab.status) == 'complete' && tab.url != undefined && tab.url != previousURL) {
+        previousURL = tab.url;
+        if (regex.test(tab.url)) {
+            redirectPage(tab);
+        } else if ((tab.url).includes("cid")) {
+            setTimeout(function() {
+                browser.tabs.reload();
+            }, 5000);
+        }
+    }
+});
--- Rerouter Extension/Resources/content.js
+++ Rerouter Extension/Resources/content.js
@@ -1,5 +1,1 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request)
-    window.location.assign(request.redirect);
-    return true;
-});
+
